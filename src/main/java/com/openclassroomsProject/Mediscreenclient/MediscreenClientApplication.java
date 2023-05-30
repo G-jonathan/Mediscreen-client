d71ca801120800d797c876e836216cc6e8b0caee
jonathan.G <jonathan.gouveia@protonmail.com>
2023-05-30 18:06:42 +0200

    first commit

---
+++ src/main/java/com/openclassroomsProject/Mediscreenclient/MediscreenClientApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroomsProject.Mediscreenclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenClientApplication.class, args);
+	}
+
+}
